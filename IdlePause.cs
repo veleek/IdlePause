816035c716f441c51531af414278aec08fd9c18e
Ben Randall <veleek@gmail.com>
2017-04-17 23:30:06 -0700

    Improve idle checking

--- IdlePause.cs
+++ IdlePause.cs
@@ -10,6 +10,7 @@ namespace Ben.StardewValley
     {
         private int idleTime;
         private int prevTimerSinceLastMovement;
+        private int prevToolIndex;
 
         public IdlePauseConfig Config { get; set; }
 
@@ -33,31 +34,53 @@ namespace Ben.StardewValley
                 return;
             }
 
-            // If the game is not paused and there's no menu open
-            if (!Game1.paused && Game1.activeClickableMenu == null)
+            if (!this.CheckIdle())
             {
-                
-                if (player.UsingTool || player.isMoving() || player.timerSinceLastMovement <= this.prevTimerSinceLastMovement)
+                // Reset the idle time if time if they're doing anything, or they've moved at all.
+                this.idleTime = 0;
+            }
+            else
+            {
+                this.idleTime += player.timerSinceLastMovement - this.prevTimerSinceLastMovement;
+
+                if (this.idleTime > this.Config.IdleDuration)
                 {
-                    // Reset the idle time if time if they're doing anything, or they've moved at all.
+                    // If we're currently idle, open the inventory
+                    Game1.activeClickableMenu = new GameMenu();
                     this.idleTime = 0;
-                }
-                else
-                {
-                    this.idleTime += player.timerSinceLastMovement - this.prevTimerSinceLastMovement;
-
-                    this.Monitor.Log($"IdleTime: {this.idleTime}");
-
-                    if (this.idleTime > this.Config.IdleDuration)
-                    {
-                        // If we're currently idle, open the inventory
-                        Game1.activeClickableMenu = new GameMenu();
-                        this.idleTime = 0;
-                    }
                 }
             }
 
             this.prevTimerSinceLastMovement = player.timerSinceLastMovement;
+            this.prevToolIndex = player.CurrentToolIndex;
+        }
+
+        /// <summary>
+        /// Determines if the player is currently idle.
+        /// </summary>
+        /// <returns>True if the player is idle, false otherwise.</returns>
+        private bool CheckIdle()
+        {
+            Farmer player = Game1.player;
+
+            // If the game is paused or there's a menu open, we don't count as idle.
+            if (Game1.paused || Game1.activeClickableMenu != null) return false;
+
+            // Check for any tool use.
+            if (player.UsingTool) return false;
+
+            // Check if they've changed tools.
+            if (player.CurrentToolIndex != this.prevToolIndex) return false;
+
+            // Check for movement (or attempted movement)
+            if (player.CanMove && player.isMoving()) return false;
+            // This check may not be necessary. 
+            if (player.timerSinceLastMovement <= this.prevTimerSinceLastMovement) return false;
+
+            // Check for mouse movement (e.g. looking at stuff in inventory).
+            if (Game1.getOldMouseY() != Game1.getMouseY() || Game1.getOldMouseX() != Game1.getMouseX()) return false;
+
+            return true;
         }
     }
 }
